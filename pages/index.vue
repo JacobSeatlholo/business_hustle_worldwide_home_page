b12ae860c111d7eb28d9206c6919ce04a2a05c30
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-05 09:54:34 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        Business hustle
-      </h1>
-      <h2 class="subtitle">
+      <img src="~/assets/BHW logo_yt.svg" />
+      <h2 class="title">
         
       </h2>
       <div class="links">
         <a
-          href="https://bhhome.vercel.app/"
+          href="https://nuxtjs.org/"
           target="_blank"
+          rel="noopener noreferrer"
           class="button--green"
         >
           Home
         </a>
         <a
-          href="https://bhhome.vercel.app/docs/doc1.html"
+          href="https://hustleportal-git-bhw1-businesshustle.vercel.app/tutorial"
           target="_blank"
+          rel="noopener noreferrer"
           class="button--grey"
         >
-          Resources
+          CRM App
         </a>
       </div>
     </div>
@@ -29,44 +28,5 @@
 </template>
 
 <script>
-import Logo from '~/components/undraw_monitor.svg'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
